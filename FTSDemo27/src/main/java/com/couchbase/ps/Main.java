9d7db6980e7bc345329d371ca9dac10aef469580
Tyler Denton <Tyler.Denton@clearcaptions.com>
2020-03-04 11:08:38 -0700

    Added tracer to 27 project

--- FTSDemo27/src/main/java/com/couchbase/ps/Main.java
+++ FTSDemo27/src/main/java/com/couchbase/ps/Main.java
@@ -1,16 +1,20 @@
 package com.couchbase.ps;
 
+import com.couchbase.client.core.tracing.ThresholdLogReporter;
+import com.couchbase.client.core.tracing.ThresholdLogTracer;
 import com.couchbase.client.java.Bucket;
 import com.couchbase.client.java.Cluster;
 import com.couchbase.client.java.CouchbaseCluster;
 import com.couchbase.client.java.document.JsonDocument;
 import com.couchbase.client.java.document.json.JsonObject;
+import com.couchbase.client.java.query.N1qlQuery;
 import com.couchbase.client.java.query.N1qlQueryResult;
 import com.couchbase.client.java.env.CouchbaseEnvironment;
 import com.couchbase.client.java.env.DefaultCouchbaseEnvironment;
 import com.couchbase.client.java.search.SearchQuery;
 import com.couchbase.client.java.search.queries.MatchQuery;
 import com.couchbase.client.java.search.result.SearchQueryResult;
+import io.opentracing.Tracer;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -18,6 +22,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 
 public class Main {
@@ -40,7 +45,18 @@ public class Main {
         String dbClusterList = m_props.getProperty("cluster");
         List<String> nodes = Arrays.asList(dbClusterList.split("\\s*,\\s*"));
 
+        Tracer tracer = ThresholdLogTracer.create(ThresholdLogReporter.builder()
+                .kvThreshold(1, TimeUnit.MICROSECONDS)
+                .n1qlThreshold(1, TimeUnit.MICROSECONDS)
+                .ftsThreshold(1, TimeUnit.MICROSECONDS)
+                .analyticsThreshold(1, TimeUnit.MICROSECONDS)
+                .logInterval(10, TimeUnit.SECONDS)
+                .sampleSize(Integer.MAX_VALUE)
+                .pretty(true)
+                .build());
+
         CouchbaseEnvironment env = DefaultCouchbaseEnvironment.builder()
+                .tracer(tracer)
                 .mutationTokensEnabled(true)
                 .build();
 
@@ -54,7 +70,7 @@ public class Main {
         JsonDocument holdingVar = bucket.upsert(doc);
         System.out.println(holdingVar);
 
-        String indexName = "unstored";
+        String indexName = m_props.getProperty("fts_index");
         MatchQuery query = SearchQuery.match("test");
 
         SearchQueryResult result = bucket.query(
@@ -64,6 +80,19 @@ public class Main {
                 );
 
         System.out.println("Simple Text Query: " + result);
+
+        JsonDocument test_doc = bucket.get("test-doc");
+        if(test_doc != null){
+            bucket.upsert(test_doc);
+        }
+
+        N1qlQueryResult n1qlHoldingVar = bucket.query(N1qlQuery.simple("Select * from `" + m_props.getProperty("bucket") + "` limit 100;"));
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void LoadProperties(File f) throws IOException {
